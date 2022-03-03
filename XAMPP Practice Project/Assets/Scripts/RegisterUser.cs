f8c81e85cee8076956c0e81270abafac41a2752b
pkcody <77869251+pkcody@users.noreply.github.com>
2022-03-02 23:41:04 -0700

    logins

---
+++ XAMPP Practice Project/Assets/Scripts/RegisterUser.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterUser : MonoBehaviour
+{
+    public InputField UsernameInput;
+    public InputField PasswordInput;
+    public InputField ConfirmPasswordInput;
+    public Button SubmitButton;
+
+    void Start()
+    {
+        SubmitButton.onClick.AddListener(() => {
+            if (PasswordInput.text == ConfirmPasswordInput.text) {
+                StartCoroutine(Main.Instance.Web.RegisterUser(UsernameInput.text, PasswordInput.text));
+            }
+            else
+            {
+                Debug.Log("Life sucks and your passwords don't match. Awkwardddd");
+            }
+        });
+    }
+}
